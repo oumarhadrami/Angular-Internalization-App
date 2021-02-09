b1fb170df358bbe7f62cb583b5694a85e33b33f6
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-02-09 11:38:35 +0000

    Add Arabic Language Support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,12 +8,24 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'nl']);
+  constructor(
+    public translate: TranslateService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    translate.addLangs(['en', 'nl', 'ar']);
     translate.setDefaultLang('en');
   }
 
   switchLang(lang: string) {
     this.translate.use(lang);
+    if (lang == 'ar') {
+      document.getElementsByTagName('html').item(0)?.setAttribute('lang','ar');
+      document.getElementsByTagName('html').item(0)?.setAttribute('dir','rtl');
+      document.getElementById('stylesheet')?.setAttribute('href', 'assets/rtl-bootstrap/bootstrap.rtl.min.css');
+    } else {
+      document.getElementsByTagName('html').item(0)?.setAttribute('lang','en');
+      document.getElementsByTagName('html').item(0)?.setAttribute('dir','ltr');
+      document.getElementById('stylesheet')?.setAttribute('href', 'assets/bootstrap/bootstrap.min.css');
+    }
   }
 }
